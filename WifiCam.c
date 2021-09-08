b6338657ef12f9a30aee4cd61d456a8363627482
RamadhanZain <60983712+ramadhan-zain@users.noreply.github.com>
2021-09-08 16:31:53 +0700

    Update WifiCam.c

--- WifiCam.c
+++ WifiCam.c
@@ -19,34 +19,6 @@ WebServer server(80);
 static auto loRes = esp32cam::Resolution::find(320, 240);
 static auto hiRes = esp32cam::Resolution::find(800, 600);
 
-void handleBmp()
-{
-  if (!esp32cam::Camera.changeResolution(loRes)) {
-    Serial.println("SET-LO-RES FAIL");
-  }
-
-  auto frame = esp32cam::capture();
-  if (frame == nullptr) {
-    Serial.println("CAPTURE FAIL");
-    server.send(503, "", "");
-    return;
-  }
-  Serial.printf("CAPTURE OK %dx%d %db\n", frame->getWidth(), frame->getHeight(),
-                static_cast<int>(frame->size()));
-
-  if (!frame->toBmp()) {
-    Serial.println("CONVERT FAIL");
-    server.send(503, "", "");
-    return;
-  }
-  Serial.printf("CONVERT OK %dx%d %db\n", frame->getWidth(), frame->getHeight(),
-                static_cast<int>(frame->size()));
-
-  server.setContentLength(frame->size());
-  server.send(200, "image/bmp");
-  WiFiClient client = server.client();
-  frame->writeTo(client);
-}
 
 void serveJpg()
 {
@@ -63,14 +35,6 @@ void serveJpg()
   server.send(200, "image/jpeg");
   WiFiClient client = server.client();
   frame->writeTo(client);
-}
-
-void handleJpgLo()
-{
-  if (!esp32cam::Camera.changeResolution(loRes)) {
-    Serial.println("SET-LO-RES FAIL");
-  }
-  serveJpg();
 }
 
 void handleJpgHi()
